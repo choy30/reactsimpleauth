24bc91eb55ea7c3a9630a1225112c97f1c196a91
devYeonjun <alcls0026@gmail.com>
2022-05-22 23:05:45 +1200

    Add redirecting the user usehistory

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,9 +1,11 @@
 import { useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 import AuthContext from "../../store/auth-context";
 import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+	const history = useHistory();
 	const newPasswordInputRef = useRef();
 	const authCtx = useContext(AuthContext);
 	const submitHandler = (e) => {
@@ -24,7 +26,9 @@ const ProfileForm = () => {
 					"Content-Type": "application/json",
 				},
 			}
-		).then((res) => {});
+		).then((res) => {
+			history.replace("/");
+		});
 	};
 	return (
 		<form className={classes.form} onSubmit={submitHandler}>
