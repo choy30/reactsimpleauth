7bedb8a3500103ae459b6ea7c743c105f3f9f111
devYeonjun <alcls0026@gmail.com>
2022-05-22 23:08:24 +1200

    Add logout

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -7,6 +7,10 @@ import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
 	const authCtx = useContext(AuthContext);
 	const isLoggedIn = authCtx.isLoggedIn;
+	const loggoutHandler = () => {
+		authCtx.logout();
+		//redirect the user
+	};
 	return (
 		<header className={classes.header}>
 			<Link to="/">
@@ -26,7 +30,7 @@ const MainNavigation = () => {
 					)}
 					{isLoggedIn && (
 						<li>
-							<button>Logout</button>
+							<button onClick={loggoutHandler}>Logout</button>
 						</li>
 					)}
 				</ul>
